48ac19c115f389288b3acbe131e75b4df8a6ba4f
Dan <dpete016@gmail.com>
2019-04-29 20:55:33 -0400

    first commit

---
+++ clickygame/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+
+
+function Navbar() {
+  return (
+   <nav className="navbar">
+     <ul>
+       <li className="brand">
+         <h1>Score: </h1> 
+        </li>
+     </ul>
+   </nav>
+     
+  );
+}
+  
+export default Navbar;
---
+++ clickygame/src/components/Section.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Section.css";
+
+
+
+function Section() {
+    return (
+       <div className="card">
+         <div className="img-container">
+          {/* <img alt={props.name} src={props.name} /> */}
+         </div>
+       </div>
+    );
+};
+
+export default Section;
